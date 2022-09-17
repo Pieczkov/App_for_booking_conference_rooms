ed8c9698fae96aa251bb7424a35ecab9504f42ca
Grzegorz <pieczaragrzegorz@gmail.com>
2022-09-17 18:09:23 +0200

    git ignore changed

--- booking_app/views.py
+++ booking_app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from booking_app.models import ConferenceRoom, RoomReservation
 
+
 """Function that redirects to the home page"""
 
 
@@ -11,11 +12,9 @@ def home(request):
     return render(request, "home.html")
 
 
-"""The main class responsible for adding conference rooms 
-and dynamically completing the page with the list of rooms"""
-
-
 class AddConferenceRoomView(View):
+    """The main class responsible for adding conference rooms
+    and dynamically completing the page with the list of rooms"""
 
     def get(self, request):
         return render(request, "add_conference_room.html")
@@ -31,28 +30,23 @@ class AddConferenceRoomView(View):
         if capacity < 0:
             return render(request, "add_conference_room.html", context={"error": "Capacity must be above zero"})
         if ConferenceRoom.objects.filter(room_name=name):
-            return render(request, "add_conference_room.html", context={"error": "The name of the room is already taken"})
+            return render(request, "add_conference_room.html",
+                          context={"error": "The name of the room is already taken"})
         ConferenceRoom(room_name=name, room_capacity=capacity, projector=projector).save()
-        #return render(request, "conference_room_list.html")
-                      # {'message': f"Added a conference room {name} with capacity {capacity}"})
         return redirect("/conference_room_list/")
 
 
-"""A class that takes all the objects from the ConferenceRoom model 
-and passes them to the html template"""
-
-
 class ConferenceRoomListView(View):
+    """A class that takes all the objects from the ConferenceRoom model
+    and passes them to the html template"""
 
     def get(self, request):
         conference_rooms = ConferenceRoom.objects.all()
         return render(request, "conference_room_list.html", context={"conference_rooms": conference_rooms})
 
 
-"""Class which delete the object"""
-
-
 class ConferenceRoomDeleteView(View):
+    """Class which delete the object"""
 
     def get(self, request, room_id):
         room_to_delete = ConferenceRoom.objects.get(id=room_id)
@@ -60,10 +54,8 @@ class ConferenceRoomDeleteView(View):
         return redirect("/conference_room_list/")
 
 
-"""Class enabling modification of conference rooms"""
-
-
 class ConferenceRoomModificationView(View):
+    """Class enabling modification of conference rooms"""
 
     def get(self, request, room_id):
         room_to_change = ConferenceRoom.objects.get(id=room_id)
@@ -73,17 +65,17 @@ class ConferenceRoomModificationView(View):
         name = request.POST.get("room_name")
         capacity = request.POST.get("room_capacity")
         capacity = int(capacity) if capacity else 0
-        projector = request.POST.get("projector") == "on"  #zamiast ifuw dodac == "on" bo chceck box zwraca nona gdy sie nie nic nie zaznaczy
+        projector = request.POST.get(
+            "projector") == "on"  # zamiast ifuw dodac == "on" bo chceck box zwraca nona gdy sie nie nic nie zaznaczy
         if name == "":
             return render(request, "conference_room_list.html", context={"error": "The name of the conference room is "
-                                                                                 "essential"})
+                                                                                  "essential"})
         if capacity < 0:
             return render(request, "conference_room_list.html", context={"error": "Capacity must be above zero"})
-        if ConferenceRoom.objects.filter(room_name=name):   #tego ni potrzeba chyba potem to przemysle
-            return render(request, "conference_room_list.html", context={"error": "The name of the room is already taken"})
+        if ConferenceRoom.objects.filter(room_name=name):  # tego ni potrzeba chyba potem to przemysle
+            return render(request, "conference_room_list.html",
+                          context={"error": "The name of the room is already taken"})
         ConferenceRoom(id=room_id, room_name=name, room_capacity=capacity, projector=projector).save()
-        # return render(request, "add_conference_room.html",
-        #               {'message': f"Added a conference room {name} with capacity {capacity}"})
         return redirect("/conference_room_list/")
 
 
@@ -103,3 +95,5 @@ class ConferenceRoomReservationView(View):
                           {"message": "the room is already reserved on the selected date"})
         RoomReservation(date=reservation_date, comment=comment, room_id_id=room_id).save()
         return redirect("/conference_room_list/")
+
+# class ConferenceRoomInfoView(View):
