8bb1472416e2d4f8dc80c90c0c83c7453d0113cd
Grzegorz <pieczaragrzegorz@gmail.com>
2022-09-17 09:23:10 +0200

    added reservation model

--- booking_app/models.py
+++ booking_app/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 
-# Create your models here.
+"""Model representing conference rooms"""
+
+
 class ConferenceRoom(models.Model):
     room_name = models.CharField(max_length=255, unique=True)
     room_capacity = models.PositiveIntegerField()
     projector = models.BooleanField(default=False)
+
+
+"""Model representing conference room reservations for a given day"""
+
+
+class RoomReservation(models.Model):
+    date = models.DateField(auto_now=True)
+    comment = models.TextField(null="no comment added")
+    room_id = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE)
+
 
 
